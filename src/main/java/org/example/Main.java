032c11a96b959c5383aa93c88b0057486db7245c
Maziyar Gerami <maziyar.gerami@connect2wow.com>
2024-06-19 05:36:12 +0330

    STEP-5: Fix - Replace the line break with space

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String greeting = "Good day!";
-        System.out.println("Hello world!" + "\n" + greeting);
+        System.out.println("Hello world!" + " " + greeting);
     }
 }
